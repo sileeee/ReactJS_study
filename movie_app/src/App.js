3183e9b7bd5c738a83b3f09d168184d185cc1d5d
tldms <tldms201@naver.com>
2020-12-26 23:52:46 +0900

    Dynamic Component Generation

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,23 +1,48 @@
 import React from 'react'
 
-function Food({fav}){ /* (props) 또는 props내부의 fav사용 가능 - ({fav})*/
+function Food({name, picture}){ /* (props) 또는 props내부의 fav사용 가능 - ({fav})*/
   // console.log(props);
-  return <h3> I like {fav}</h3>
+  return <div>
+    <h3> I like {name}</h3>
+    <img src = {picture}/>
+    </div>
+
   // return <h3> I like {props.fav}</h3>
 }
+const foodILike = [
+  {
+    name: "Kimchi",
+    image:
+      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
+  },
+  {
+    name: "Samgyeopsal",
+    image:
+      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg"
+  },
+  {
+    name: "Bibimbap",
+    image:
+      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb"
+  },
+  {
+    name: "Doncasu",
+    image:
+      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
+  },
+  {
+    name: "Kimbap",
+    image:
+      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg"
+  }
+];
 
 function App() {
   return (
     <div>
-      hello
-      <Food fav="kimchi" /*food컴포넌트에 정보(fav라는 prop(erty)+value)를 보냄 */
-      something = {true}
-      papapa = {["hello", 1, 2, true]}/>
-      <Food fav = "ramen"/>
-      
-      
+      {foodILike.map(dish => <Food name = {dish.name} picture = {dish.image}/>)}
     </div>
-    
+    // 여기서 dish는 object(name과 image를 가지고 있음)
   );
 }
 
