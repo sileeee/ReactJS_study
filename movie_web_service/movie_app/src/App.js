a6c1211df21b355bfed633543341384dfdcc7699
tldms <tldms201@naver.com>
2020-12-30 23:57:43 +0900

    Fetching Movies from API

--- movie_web_service/movie_app/src/App.js
+++ movie_web_service/movie_app/src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
+import axios from "axios";
 
 class App extends React.Component {
   state = {
     isLoading: true, //mount되자마자
     movies: [] //movie state (미래에 쓰고자하는 state를 선언하는 것은 필수가 아님)
   };
+  getMovies = async () => {
+    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+  };
   componentDidMount() { //여기서 data를 fetch함
-    setTimeout(() => { //delay function(java script)
-      this.setState({ isLoading: false }); 
-    }, 6000); //6초후 실행
+    this.getMovies();
   }
   render() {
     const { isLoading } = this.state; //es6
